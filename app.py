8d2f2f75e43b35916b94fe8912b8dc2509322304
Mowlidharan <mowlidharanarumugam@gmail.com>
2024-07-03 19:30:27 +0530

    main file

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify
 import joblib 
 app = Flask(__name__)
 
+
 # Load the model
 model = joblib.load('/home/mowli/Documents/ECG signal Classification/ECG-Signal-Classification/ecg_pred.pkl')
 
